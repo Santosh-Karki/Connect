4b48149965457413e1be3508ca53bb2e3e2dea71
santosh-karki <karki_santosh@outlook.com>
2025-05-12 18:34:37 +1000

    d

---
+++ backend/db/connDb.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+
+export const connDb = async()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log('MongoDb connected:', conn.connection.host);
+
+    }catch(error){
+        console.log('MongoDb connection error:',error.message);
+        process.exit(1);
+
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connDb } from './db/connDb';
+
+dotenv.config();
+
+const PORT = process.env.PORT
 
 const app = express();
 
 app.listen(3001, ()=>{
-    console.log('Server is running on port 3001');
+    connDb();
+    console.log('Server is running on port', PORT);
 })
