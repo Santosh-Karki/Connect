5288b71058b135dade65d74dab744ef803fe05b4
santosh-karki <karki_santosh@outlook.com>
2025-05-19 06:57:40 +1000

    sd

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,20 @@
+import User from "../models/User.js";
+
+export async function getRecommendedUsers(req, res) {
+    try {
+      const currentUserId = req.user.id;
+      const currentUser = req.user;
+  
+      const recommendedUsers = await User.find({
+        $and: [
+          { _id: { $ne: currentUserId } }, //exclude current user
+          { _id: { $nin: currentUser.friends } }, // exclude current user's friends
+          { isOnboarded: true },
+        ],
+      });
+      res.status(200).json(recommendedUsers);
+    } catch (error) {
+      console.error("Error in getRecommendedUsers controller", error.message);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
