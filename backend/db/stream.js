f1b9f7518895533cfdee0597ddfe993263c24847
santosh-karki <karki_santosh@outlook.com>
2025-05-21 17:25:13 +1000

    as

--- backend/db/stream.js
+++ backend/db/stream.js
@@ -1,2 +1,11 @@
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
+
+const apiKey = process.env.STEAM_API_KEY;
+const apiSecret = process.env.STEAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error("Stream API key or Secret is missing");
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
