ddfd03f972416649af2a997124fe8d88f70ae29a
santosh-karki <karki_santosh@outlook.com>
2025-05-22 17:50:36 +1000

    as

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
 
+
+
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
+import { connectDB } from "./lib/db.js";
 const app = express();
 dotenv.config();
 
