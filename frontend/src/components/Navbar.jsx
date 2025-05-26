3c4d6f665f4b81138da41c90533fda9186989ab1
santosh-karki <karki_santosh@outlook.com>
2025-05-27 07:57:27 +1000

    sd

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,3 +3,16 @@ import useAuthUser from "../hooks/useAuthUser";
 import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
 import ThemeSelector from "./ThemeSelector";
 import useLogout from "../hooks/useLogout";
+
+onst Navbar = () => {
+  const { authUser } = useAuthUser();
+  const location = useLocation();
+  const isChatPage = location.pathname?.startsWith("/chat");
+
+  // const queryClient = useQueryClient();
+  // const { mutate: logoutMutation } = useMutation({
+  //   mutationFn: logout,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  const { logoutMutation } = useLogout();
