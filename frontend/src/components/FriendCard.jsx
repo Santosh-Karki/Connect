386fabd82568e453784696d7572ce344f72a20bc
santosh-karki <karki_santosh@outlook.com>
2025-05-26 18:08:21 +1000

    fd

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router";
 import { LANGUAGE_TO_FLAG } from "../constants";
 
-
 const FriendCard = ({ friend }) => {
   return (
     <div className="card bg-base-200 hover:shadow-md transition-shadow">
@@ -39,3 +38,15 @@ export function getLanguageFlag(language) {
 
   const langLower = language.toLowerCase();
   const countryCode = LANGUAGE_TO_FLAG[langLower];
+
+  if (countryCode) {
+    return (
+      <img
+        src={`https://flagcdn.com/24x18/${countryCode}.png`}
+        alt={`${langLower} flag`}
+        className="h-3 mr-1 inline-block"
+      />
+    );
+  }
+  return null;
+}
