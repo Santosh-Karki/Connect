1c6e438617217df29ba92ecad34d70bfc860c014
santosh-karki <karki_santosh@outlook.com>
2025-05-29 07:57:40 +1000

    sdfs

--- frontend/src/components/ThemeCollector.jsx
+++ frontend/src/components/ThemeCollector.jsx
@@ -1,3 +1,20 @@
 import { PaletteIcon } from "lucide-react";
 import { useThemeStore } from "../store/useThemeStore";
 import { THEMES } from "../constants";
+
+
+const ThemeSelector = () => {
+  const { theme, setTheme } = useThemeStore();
+
+  return (
+    <div className="dropdown dropdown-end">
+      {/* DROPDOWN TRIGGER */}
+      <button tabIndex={0} className="btn btn-ghost btn-circle">
+        <PaletteIcon className="size-5" />
+      </button>
+
+      <div
+        tabIndex={0}
+        className="dropdown-content mt-2 p-1 shadow-2xl bg-base-200 backdrop-blur-lg rounded-2xl
+        w-56 border border-base-content/10 max-h-80 overflow-y-auto"
+      ></div>
