35f44f01a365d5a718ecb366edf6d17cd34cdbdd
santosh-karki <karki_santosh@outlook.com>
2025-06-03 07:23:14 +1000

    f;lmgsl

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -29,3 +29,14 @@ const LoginPage = () => {
     email: "",
     password: "",
   });
+
+  import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import useLogin from "../hooks/useLogin";
+
+const LoginPage = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
