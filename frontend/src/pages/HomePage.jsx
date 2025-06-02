d36c7e8538f208f586f05e044e542a022fcccd80
santosh-karki <karki_santosh@outlook.com>
2025-06-02 16:03:40 +1000

    hdf

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import {
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  getUserFriends,
+  sendFriendRequest,
+} from "../lib/api";
+import { Link } from "react-router";
+import { CheckCircleIcon, MapPinIcon, UserPlusIcon, UsersIcon } from "lucide-react";
+
+import { capitialize } from "../lib/utils";
