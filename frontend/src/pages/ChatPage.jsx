b7a8d216eb661f8f5f2fa9fc9b9fe75871ff1abf
santosh-karki <karki_santosh@outlook.com>
2025-06-02 16:02:00 +1000

    sd

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -19,3 +19,20 @@ import toast from "react-hot-toast";
 
 import ChatLoader from "../components/ChatLoader";
 import CallButton from "../components/CallButton";
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY;
+
+const ChatPage = () => {
+  const { id: targetUserId } = useParams();
+
+  const [chatClient, setChatClient] = useState(null);
+  const [channel, setChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const { authUser } = useAuthUser();
+
+  const { data: tokenData } = useQuery({
+    queryKey: ["streamToken"],
+    queryFn: getStreamToken,
+    enabled: !!authUser, // this will run only when authUser is available
+  });
