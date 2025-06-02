424387de07ef1c3b9e1075c3a97df84faeaf8aa8
santosh-karki <karki_santosh@outlook.com>
2025-06-02 16:01:41 +1000

    fsd

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,3 +3,19 @@ import { useParams } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStreamToken } from "../lib/api";
+
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import { StreamChat } from "stream-chat";
+import toast from "react-hot-toast";
+
+import ChatLoader from "../components/ChatLoader";
+import CallButton from "../components/CallButton";
