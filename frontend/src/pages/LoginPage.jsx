3ea1e1f1f8842958062e1a9018c95956d809d0c8
santosh-karki <karki_santosh@outlook.com>
2025-06-03 07:23:27 +1000

    klaklfm

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -40,3 +40,14 @@ const LoginPage = () => {
     email: "",
     password: "",
   });
+
+  import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import useLogin from "../hooks/useLogin";
+
+const LoginPage = () => {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
