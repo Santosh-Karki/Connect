75762c453a59f2e57e177092cac23f2104aa51b5
santosh-karki <karki_santosh@outlook.com>
2025-06-04 17:16:22 +1000

    nams,d

---
+++ frontend/src/pages/NotificationsPage.jsx
@@ -0,0 +1,20 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { acceptFriendRequest, getFriendRequests } from "../lib/api";
+import { BellIcon, ClockIcon, MessageSquareIcon, UserCheckIcon } from "lucide-react";
+import NoNotificationsFound from "../components/NoNotificationsFound";
+
+const NotificationsPage = () => {
+  const queryClient = useQueryClient();
+
+  const { data: friendRequests, isLoading } = useQuery({
+    queryKey: ["friendRequests"],
+    queryFn: getFriendRequests,
+  });
+
+  const { mutate: acceptRequestMutation, isPending } = useMutation({
+    mutationFn: acceptFriendRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["friendRequests"] });
+      queryClient.invalidateQueries({ queryKey: ["friends"] });
+    },
+  });
