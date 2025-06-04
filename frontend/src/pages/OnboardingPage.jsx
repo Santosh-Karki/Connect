3db48a079e6c1664a6403b9a4075f1561fb4dfc8
santosh-karki <karki_santosh@outlook.com>
2025-06-05 07:12:38 +1000

    sd

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -4,4 +4,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { completeOnboarding } from "../lib/api";
 import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
-import { LANGUAGES } from "../constants";
+import { LANGUAGES } from "../constants";const OnboardingPage = () => {
+  const { authUser } = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
