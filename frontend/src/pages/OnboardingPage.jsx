bb5d744e5af090a0dbadaecdc19915599b575f59
santosh-karki <karki_santosh@outlook.com>
2025-06-05 07:12:20 +1000

    sd

---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { completeOnboarding } from "../lib/api";
+import { LoaderIcon, MapPinIcon, ShipWheelIcon, ShuffleIcon } from "lucide-react";
+import { LANGUAGES } from "../constants";
