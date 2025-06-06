b31e2c23c165e695eab044f1dab664634b377686
santosh-karki <karki_santosh@outlook.com>
2025-06-07 06:51:05 +1000

    fad

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -61,3 +61,24 @@ const SignUpPage = () => {
   //   mutationFn: signup,
   //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
   // });
+
+
+  import useSignUp from "../hooks/useSignUp";
+
+const SignUpPage = () => {
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: signupMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
